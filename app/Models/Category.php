4e879299f3b4e981823802798375824ab0d39516
Yumiko Shiga <totosmy2718@gmail.com>
2026-09-02 10:58:59 +0800

    feat: モデル・リレーション実装 #3

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'content',
+    ];
+    /**
+     * このカテゴリーに属する問い合わせを取得
+     */
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
+
+
